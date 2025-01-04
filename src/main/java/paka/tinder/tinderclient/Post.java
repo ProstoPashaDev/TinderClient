68158524910d9ae8cbf32a27020349bfea32d238
Hramov2 <hramovpi@gmail.com>
2025-01-04 12:09:47 +0300

    Commit with User and Post classes

---
+++ src/main/java/paka/tinder/tinderclient/Post.java
@@ -0,0 +1,42 @@
+package paka.tinder.tinderclient;
+
+public class Post {
+
+    private int postId;
+    private int userId;
+    private String text;
+    private String image;
+
+    public int getPostId() {
+        return postId;
+    }
+
+    public void setPostId(int postId) {
+        this.postId = postId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
+
