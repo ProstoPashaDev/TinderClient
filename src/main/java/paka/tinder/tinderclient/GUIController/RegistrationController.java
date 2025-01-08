f420c9640eba988438203ce5ceeb9f598d3e0b3e
mao72 <kirillmao27@icloud.com>
2025-01-08 15:08:55 +0400

    some frontend shit

--- src/main/java/paka/tinder/tinderclient/GUIController/RegistrationController.java
+++ src/main/java/paka/tinder/tinderclient/GUIController/RegistrationController.java
@@ -7,11 +7,17 @@ import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import paka.tinder.tinderclient.ExampleApplication;
+import paka.tinder.tinderclient.User;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class RegistrationController {
-
+    static boolean validEmail(String email) {
+        // return email.matches("[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,4}");
+        return email.matches("^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@" +
+                "(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$");
+    }
     @FXML
     private Label greetingText;
 
@@ -39,6 +45,27 @@ public class RegistrationController {
     @FXML
     protected void setSignUpButton() throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(ExampleApplication.class.getResource("authorization.fxml"));
+        String emailAddress = loginField.getText().trim();
+        String password = passwordField.getText();
+        String passwordCheck = passwordCheckField.getText();
+        if (!validEmail(emailAddress)) {
+            greetingText.setText("Please enter valid email address");
+            return;
+        }
+        if (password.isEmpty()) {
+            greetingText.setText("Please enter valid password");
+            return;
+        }
+        if (!Objects.equals(passwordCheck, password)) {
+            greetingText.setText("Passwords are not the same");
+        }
+        //проверка на пароль нужной длины, наличие спец символов, цифр, крч обсудим
+        User user = new User();
+        user.setEmail(emailAddress);
+        user.setPassword(password);
+        // короче сюда впишем some shit with database
+
+        //
         signUpButton.getScene().setRoot(fxmlLoader.load());
     }
 }
