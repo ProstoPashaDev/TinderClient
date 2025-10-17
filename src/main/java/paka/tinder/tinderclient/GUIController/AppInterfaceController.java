97d490585ca8a46c1058cfd53d42fd4179c1ea60
Hramov2 <hramovpi@gmail.com>
2025-10-17 13:52:08 +0800

    Feature. Setup friends system

--- src/main/java/paka/tinder/tinderclient/GUIController/AppInterfaceController.java
+++ src/main/java/paka/tinder/tinderclient/GUIController/AppInterfaceController.java
@@ -1,4 +1,37 @@
 package paka.tinder.tinderclient.GUIController;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+
 public class AppInterfaceController {
+    @FXML
+    BorderPane mainBorderPane;
+
+    @FXML
+    BorderPane chatBorderPane;
+
+    @FXML
+    VBox friendsListContainer;
+
+    @FXML
+    VBox usersListContainer;
+
+    public void showChat() {
+        mainBorderPane.setCenter(chatBorderPane);
+    }
+
+    public void showProfile() {
+
+    }
+
+    public void showNews() {
+
+    }
+
+    public void showFriends() {
+        friendsListContainer.setVisible(true);
+        usersListContainer.setVisible(true);
+    }
 }
